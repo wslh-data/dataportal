e4604884642d67ce134ddfe06860ca2592b445c7
Kelsey Florek <kelseyflorek22@gmail.com>
2024-11-20 15:45:30 -0600

    fixed but and status codes

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,14 @@
+
+const apiURL = 'https://3d1su84o30.execute-api.us-east-2.amazonaws.com/v1/contact'
+const dev_apiURL = 'https://xcjq92fu67.execute-api.us-east-2.amazonaws.com/v1/contact'
+const grecaptchaSiteKey = '6LcO5FAiAAAAACC5DYTxi6AmLYQ7TFFKLW2tjOfI'
+const dev_grecaptchaSiteKey = '6LfJAX4qAAAAAGPZ7D6DTh7u6zlXULTlP_8W5ubk'
+
+function onloadCallback() {
+    grecaptcha.render(document.getElementById("grecaptcha"), {'sitekey' : grecaptchaSiteKey, 'theme' : 'dark'});
+};
+
+
 $(function() {
 
     $("input,textarea").jqBootstrapValidation({
@@ -6,7 +17,11 @@ $(function() {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+            // prevent default submit behavior
+            event.preventDefault(); 
+            // disable button to prevent multiple submissions
+            document.getElementById("submitBtn").disabled = true; 
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -46,23 +61,44 @@ $(function() {
                 crossDomain: "true",
                 contentType: "application/json; charset=utf-8",
                 data: JSON.stringify(data),
+                statusCode: {
+                    200: function() {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Thanks for contacting us "+name+", your message has been sent. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
 
-                success: function () {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                    grecaptcha.reset();
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                        
+                    },
+                    406: function() {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that there is an issue with your submission.");
+                        $('#success > .alert-danger').append('</div>');
+                    },
+                    500: function() {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + name + ", there is an issue with the server.");
+                        $('#success > .alert-danger').append('</div>');
+                    }
                 },
-
-                error: function () {
+                success: function() {
+                    grecaptcha.reset();
+                    $(".loader").hide();
+                    document.getElementById("submitBtn").disabled = false;
+                },
+                error: function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -71,7 +107,8 @@ $(function() {
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     grecaptcha.reset();
-                    //$('#contactForm').trigger("reset");
+                    $(".loader").hide();
+                    document.getElementById("submitBtn").disabled = false;
                 }
             });
         }
